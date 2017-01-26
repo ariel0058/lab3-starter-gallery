421b15a51c5314c7fe7fda5679bb22c79ba46b78
ariel0058 <ariel0058@hotmail.com>
2017-01-25 18:16:07 -0800

    Basic of good web conversion

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -5,23 +5,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Gallery extends Application
 {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		//$this->load->view('gallery');
-            $this->data['pagebody'] = 'gallery';
-            $this->render();
-	}
+    /**
+     * Index Page for this controller.
+     */
+    public function index()
+    {
+        //get all the images of formatted cells for them
+        $pix = $this->images->all();
+
+        //build an array of formatted cells for them
+        foreach ($pix as $picture){
+            $cells[]= $this->parser->parse('_cell', (array) $picture, true);
+        }
+
+        //prime the table class
+        $this->load->library('table');
+        $prams = array(
+            'table_open' => '<table class="gallary">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'
+        );
+
+        $this->table->set_template($prams);
+
+        //finally, generate the table
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable']=$this->table->generate($rows);
+
+        $this->data['pagebody'] = 'gallery';
+        $this->render();
+    }
 
 }
